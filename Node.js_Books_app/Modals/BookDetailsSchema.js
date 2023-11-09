4b281938c868489601109ed4ee4216d6b97bf288
JeganTechCode <150325026+JeganTechCode@users.noreply.github.com>
2023-11-09 18:57:13 +0530

    Delete Node.js_Books_app directory

--- Node.js_Books_app/Modals/BookDetailsSchema.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
-var BooksDetails = new Schema({
-    title: String,
-    author: String,
-    summary: String,
-}, { timestamps: true })
-
-module.exports = mongoose.model("BooksDetails", BooksDetails, "BooksDetails")
--- Node.js_Books_app/config/Config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    dbName: "Book",
-    mongodburi: "mongodb://localhost:27017/<database>",
-}
